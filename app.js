4b7dce3a551b2fd30a5b494cb6e82c50cf58c25a
Mass221 <90098218+Mass221@users.noreply.github.com>
2025-08-12 01:32:38 +0000

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require('express-session');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
